3b13e003aac7a1d4421f51e462423b9cc8fc2930
Abraham Huacchillo <huacchillocastillo@gmail.com>
2022-10-19 16:15:17 -0500

    implement functions in entries.components

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -3,3 +3,74 @@ import axios from 'axios';
 import {Button,Form,Container,Modal} from 'react-bootstrap';
 import Entry from './single-entry.components';
 
+const Entries =()=>{
+    const [entries,setEntries] = useState([])
+    const [refreshData,setRefreshData]=useState(false)
+    const [changeEntry,setChangeEntry] = useState({"change":false, "id":0})
+    const [changeIngredient, setChangeIngredient] = useState({"change":false, "id":0})
+    const [newIngredientsName, setNewIngredientsName] = useState("")
+    const [addNewEntry, setAddNewEntry] = useState(false)
+    const [newEntry,setNewEntry] = useState({"dish":"","ingredients":"","calories":0,"fat":0})
+
+
+    useEffect(() =>{
+        getAllEntries();
+    },[])
+
+    if(refreshData){
+        setRefreshData(false);
+        getAllEntries();
+    }
+
+
+    return(
+        <div>
+            <Container>
+                <Button onClick={()=> setAddNewEntry(true)}>Track Today's calories</Button>
+            </Container>
+
+            <Container>
+                {entries != null && entries.map((entry,i)=>{
+                    <Entry entryData={entry} deleteSingleEntry ={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry} />
+                })}
+            </Container>
+        </div>
+    );
+}
+
+function addSingleEntry(){
+    setAddNewEntry(false);
+    var url = "http://localhost:8000/entry/create"
+    axios.post(url,{
+        "ingredients":newEntry.ingredients,
+        "dish":newEntry.dish,
+        "calories":newEntry.calories,
+        "fat":parseFloat(newEntry.fat)
+    }).then(response => {
+        if (response.status == 200){
+            setRefreshData(true)
+        }
+    })
+}
+
+function deleteSingleEntry(id){
+    var url ="http://localhost:8000/entry/delete" + id
+    axios.delete(url,{
+
+    }).then (response => {
+        if (response.status == 200){
+            setRefreshData(true)
+        }
+    })
+}
+
+function getAllEntries(){
+    var url = "http://localhost:8000/entries"
+    axios.get(url,{
+        responseType: "json",
+    }).then(response => {
+        if(response.status == 200){
+            setEntries(response.data)
+        }
+    })
+}
