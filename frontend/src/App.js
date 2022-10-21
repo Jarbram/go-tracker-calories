2c0d225791d3a9e7e642173aeebeda3a974557a6
Abraham Huacchillo <huacchillocastillo@gmail.com>
2022-10-21 15:25:49 -0500

    organizing documents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import  'bootstrap/dist/css/bootstrap.css'
 
 import Entries from './components/entries.components'
-import Entry from './components/single-entry.components';
-
 function App() {
   return (
     <div className="App">
